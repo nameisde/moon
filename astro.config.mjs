5d648386bd0b6d72d1edd0156db1eac926545e30
nameisde <nameisde@gmail.com>
2022-12-04 13:52:42 +0700

    🧩 Install MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs(), mdx()]
 });
