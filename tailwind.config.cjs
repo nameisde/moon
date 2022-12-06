881d6f4891917afe30f79f895c33d475dea1cd6a
nameisde <nameisde@gmail.com>
2022-12-06 16:56:13 +0700

    + Add font family and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        fontFamily: {
+            sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        },
+        colors: {
+            'moon': 'hsl(0, 0%, 9%, 1)',
+            'moon-default': 'hsl(0, 0%, 14%, 1)',
+            'moon-hover': 'hsl(0, 0%, 31%, 1)',
+            'moon-outline-default': 'hsl(0, 0%, 24%, 1)',
+            'moon-outline-hover': 'hsl(0, 0%, 31%, 1)',
+            'moon-text-default': 'hsl(0, 0%, 63%, 1)',
+            'moon-text-hover': 'hsl(0, 0%, 93%, 1)',
+        }
+    },
+    plugins: [
+    
+    ],
 }
+
