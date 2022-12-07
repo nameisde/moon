fb54809b3b27b66c0fa14c41025fd3e79ab1e990
nameisde <nameisde@gmail.com>
2022-12-08 00:13:08 +0700

    + Update something

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-import alpinejs from "@astrojs/alpinejs";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [tailwind({ config: { applyBaseStyles: false }, }), alpinejs(), mdx(), sitemap()]
+  site: 'https://example.com',
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx(), sitemap(), svelte()]
 });
